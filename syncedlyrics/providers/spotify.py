d5a53bf2ef9f75bba060571cb2ce2985c60a4861
Momo <lo3me@proton.me>
2022-11-27 21:13:32 +0330

    Fix class methods

--- syncedlyrics/providers/spotify.py
+++ syncedlyrics/providers/spotify.py
@@ -3,6 +3,7 @@
 from typing import Optional
 from .base import LRCProvider
 
+
 class Spotify(LRCProvider):
     """Spotify provider class"""
 
@@ -10,11 +11,11 @@ class Spotify(LRCProvider):
         super().__init__()
 
     @classmethod
-    def get_track_id(search_term : str) -> Optional[str]:
+    def get_track_id(search_term: str) -> Optional[str]:
         """Returns a Spotify track ID for given `search_term`"""
         # TODO: self.client.search(search_term) and processing the results
         pass
 
-    def get_lrc(search_term: str) -> Optional[str]:
+    def get_lrc(self, search_term: str) -> Optional[str]:
         # TODO: Use https://github.com/akashrchandran/spotify-lyrics-api
         return super().get_lrc()
