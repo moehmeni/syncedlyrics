86f15925fc898bd6f78e1e6b49a5046bd522135b
Momo <lo3me@proton.me>
2023-01-29 20:00:27 +0330

    Seperate bs4 generator

--- syncedlyrics/providers/lyricsify.py
+++ syncedlyrics/providers/lyricsify.py
@@ -1,9 +1,10 @@
 """Lyricsify (lyricsify.com) LRC provider"""
 
 from typing import Optional
-from bs4 import BeautifulSoup, SoupStrainer, FeatureNotFound
+from bs4 import BeautifulSoup, SoupStrainer
 from .base import LRCProvider
 import rapidfuzz
+from ..utils import generate_bs4_soup
 
 
 class Lyricsify(LRCProvider):
@@ -17,25 +18,22 @@ class Lyricsify(LRCProvider):
         self.parser = "html.parser"
 
     def get_lrc(self, search_term: str) -> Optional[str]:
-        endpoint = self.SEARCH_ENDPOINT + search_term.replace(" ", "+")
-        r = self.session.get(endpoint)
+        search_page_url = self.SEARCH_ENDPOINT + search_term.replace(" ", "+")
+
         # Just processing the `a` tags whose `href` attribute starts with /lyric/
         # and whose text is similar to the query too. https://github.com/maxbachmann/RapidFuzz#scorers
         _t = lambda s: s.lower().replace("-", "")
         text_match = lambda t: rapidfuzz.fuzz.token_sort_ratio(_t(search_term), _t(t))
         href_match = lambda h: h.startswith("/lyric/")
         a_tags_boud = SoupStrainer("a", href=href_match)
-        # Try using `lxml` if it's installed (it's much faster)
-        try:
-            soup = BeautifulSoup(r.text, features="lxml", parse_only=a_tags_boud)
-            self.parser = "lxml"
-        except FeatureNotFound:
-            soup = BeautifulSoup(r.text, features="html.parser", parse_only=a_tags_boud)
+        soup = generate_bs4_soup(self.session, search_page_url, parse_only=a_tags_boud)
         a_tag = soup.find_all("a", string=lambda t: text_match(t) > 80, limit=4)
         if not a_tag:
             return None
+
         # The one with the highest similarity score
         a_tag = sorted(a_tag, key=lambda tag: text_match(tag.string))[-1]
+
         # Fetching and extracting the LRC page
         r = self.session.get(self.ROOT_URL + a_tag["href"])
         lyrics_id = a_tag["href"].split(".")[-1]
--- syncedlyrics/utils.py
+++ syncedlyrics/utils.py
@@ -1,5 +1,7 @@
 """Utility functions for `syncedlyrics` package"""
 
+from bs4 import BeautifulSoup, FeatureNotFound
+
 
 def is_lrc_valid(lrc: str, allow_plain_format: bool = False) -> bool:
     """Checks whether a given LRC string is valid or not."""
@@ -16,3 +18,15 @@ def save_lrc_file(path: str, lrc_text: str):
     """Saves the `.lrc` file"""
     with open(path, "w", encoding="utf-8") as f:
         f.write(lrc_text)
+
+
+def generate_bs4_soup(session, url: str, **kwargs):
+    """Returns a `BeautifulSoup` from the given `url`.
+    Tries to use `lxml` as the parser if available, otherwise `html.parser`
+    """
+    r = session.get(url)
+    try:
+        soup = BeautifulSoup(r.text, features="lxml", **kwargs)
+    except FeatureNotFound:
+        soup = BeautifulSoup(r.text, features="html.parser", **kwargs)
+    return soup
