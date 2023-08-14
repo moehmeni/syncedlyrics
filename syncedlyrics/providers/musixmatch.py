ca3d4603fd74f283d8af62795be8734e92aa90ed
Mohammad Momeni <lo3me@pm.me>
2023-08-14 20:10:54 +0200

    Add dynamic tokens + caching

--- syncedlyrics/providers/musixmatch.py
+++ syncedlyrics/providers/musixmatch.py
@@ -1,19 +1,72 @@
 """Musixmatch LRC provider"""
 
-from typing import Optional
+from typing import Optional, List
 from .base import LRCProvider
+import time
+import json
+import os
+
+# Inspired from https://github.com/Marekkon5/onetagger/blob/0654131188c4df2b4b171ded7cdb927a4369746e/crates/onetagger-platforms/src/musixmatch.rs
+# Huge part converted from Rust to Py by ChatGPT :)
 
 
 class Musixmatch(LRCProvider):
     """Musixmatch provider class"""
 
-    USER_TOKEN = "190523f77464fba06fa5f82a9bfab0aa9dc201244ecf5124a06d95"
-    SEARCH_ENDPOINT = "https://apic-desktop.musixmatch.com/ws/1.1/track.search?format=json&q={q}&page_size=5&page=1&s_track_rating=desc&quorum_factor=1.0&app_id=web-desktop-app-v1.0&usertoken={token}"
-    LRC_ENDPOINT = "https://apic-desktop.musixmatch.com/ws/1.1/track.subtitle.get?format=json&track_id={track_id}&subtitle_format=lrc&app_id=web-desktop-app-v1.0&usertoken={token}"
+    ROOT_URL = "https://apic-desktop.musixmatch.com/ws/1.1/"
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.token = None
+        self.session.headers.update(
+            {
+                "authority": "apic-desktop.musixmatch.com",
+                "cookie": "AWSELBCORS=0; AWSELB=0",
+            }
+        )
+
+    def _get(self, action: str, query: List[tuple]):
+        if action != "token.get" and self.token is None:
+            self._get_token()
+        query.append(("app_id", "web-desktop-app-v1.0"))
+        if self.token is not None:
+            query.append(("usertoken", self.token))
+        t = str(int(time.time() * 1000))
+        query.append(("t", t))
+        url = self.ROOT_URL + action
+        response = self.session.get(url, params=query)
+        return response
+
+    def _get_token(self):
+        # Check if token is cached and not expired
+        token_path = os.path.join(".syncedlyrics", "musixmatch_token.json")
+        current_time = int(time.time())
+        if os.path.exists(token_path):
+            with open(token_path, "r") as token_file:
+                cached_token_data = json.load(token_file)
+            cached_token = cached_token_data.get("token")
+            expiration_time = cached_token_data.get("expiration_time")
+            if cached_token and expiration_time and current_time < expiration_time:
+                self.token = cached_token
+                return
+        # Token not cached or expired, fetch a new token
+        d = self._get("token.get", [("user_language", "en")]).json()
+        if d["message"]["header"]["status_code"] == 401:
+            time.sleep(10)
+            return self._get_token()
+        new_token = d["message"]["body"]["user_token"]
+        expiration_time = current_time + 600  # 10 minutes expiration
+        # Cache the new token
+        self.token = new_token
+        token_data = {"token": new_token, "expiration_time": expiration_time}
+        os.makedirs(".syncedlyrics", exist_ok=True)
+        with open(token_path, "w") as token_file:
+            json.dump(token_data, token_file)
 
     def get_lrc_by_id(self, track_id: str) -> Optional[str]:
-        url = self.LRC_ENDPOINT.format(track_id=track_id, token=self.USER_TOKEN)
-        r = self.session.get(url)
+        r = self._get(
+            "track.subtitle.get", [("track_id", track_id), ("subtitle_format", "lrc")]
+        )
         if not r.ok:
             return
         body = r.json()["message"]["body"]
@@ -22,14 +75,16 @@ class Musixmatch(LRCProvider):
         return body["subtitle"]["subtitle_body"]
 
     def get_lrc(self, search_term: str) -> Optional[str]:
-        url = self.SEARCH_ENDPOINT.format(q=search_term, token=self.USER_TOKEN)
-        r = self.session.get(url)
-        if not r.ok:
-            return
+        r = self._get(
+            "track.search",
+            [
+                ("q", search_term),
+                ("page_size", "5"),
+                ("page", "1"),
+                ("s_track_rating", "desc"),
+                ("quorum_factor", "1.0"),
+            ],
+        )
         body = r.json()["message"]["body"]
-        if not body:
-            return
         tracks = body["track_list"]
-        if not tracks:
-            return
         return self.get_lrc_by_id(tracks[0]["track"]["track_id"])
