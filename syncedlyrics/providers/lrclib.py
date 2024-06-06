e2c57e73e871255e7822d775813233e3413d1e8e
JMaximusIX <nix@gibts>
2024-06-06 15:27:10 +0200

    use best match instead of first synced lyrics

--- syncedlyrics/providers/lrclib.py
+++ syncedlyrics/providers/lrclib.py
@@ -35,13 +35,13 @@ class Lrclib(LRCProvider):
         tracks = sort_results(
             tracks, search_term, lambda t: f'{t["artistName"]} - {t["trackName"]}'
         )
-        # _id = str(tracks[0]["id"])
+        _id = str(tracks[0]["id"])
         # Getting the first track that its `syncedLyrics` is not empty
-        _id = None
-        for track in tracks:
-            if (track.get("syncedLyrics", "") or "").strip():
-                _id = str(track["id"])
-                break
-        if not _id:
-            return None
+        # _id = None
+        # for track in tracks:
+        #     if (track.get("syncedLyrics", "") or "").strip():
+        #         _id = str(track["id"])
+        #         break
+        # if not _id:
+        #     return None
         return self.get_lrc_by_id(_id)
